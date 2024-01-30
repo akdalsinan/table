bf8fe7ceac002e75809be7032b71b675c20e444b
Sinan Akdal <snnakdal@gmail.com>
2024-01-30 17:38:26 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ function App() {
   const data = [
     {
       key: "1",
-      name: "John Brown",
+      name: "John Brownsdfs",
       age: 32,
       address: "New York No. 1 Lake Park",
       tags: "nice",
     },
     {
       key: "2",
-      name: "Jim Green",
+      name: "Jim Greesfddsfn",
       age: 42,
       address: "London No. 1 Lake Park",
       tags: "loser",
     },
     {
       key: "3",
-      name: "Joe Black",
+      name: "Joe Bldsfdsack",
       age: 32,
       address: "Sydney No. 1 Lake Park",
       tags: "cool",
