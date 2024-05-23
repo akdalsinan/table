5749f44ca7195e4bdd55276be7035a24d0ee58ef
Sinan Akdal <snnakdal@gmail.com>
2024-05-23 11:14:35 +0300

    readme updatee

--- README.md
+++ README.md
@@ -3,8 +3,38 @@
 React vite kullanarak oluşturmuş olduğum STable komponenti
 
 **API Table**
+
 ![alt text](./public/tableAPI.PNG)
 
 **Install**
 
 `npm i tables7`
+
+**Usage**
+
+`import STable from "tables7/src/STable`
+
+```const usersData = [
+    {
+      key: "1",
+      name: "John Brownsdfs",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      tags: "nice",
+    },
+    {
+      key: "2",
+      name: "Jim Greesfddsfn",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      tags: "loser",
+    },
+    {
+      key: "3",
+      name: "Joe Bldsfdsack",
+      age: 32,
+      address: "Sydney No. 1 Lake Park",
+      tags: "cool",
+    },
+  ]
+```
--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,53 @@ import STable from "./STable";
 import { useSelector } from "react-redux";
 
 import { TableProps, columnsTableProps } from "./data/tableProps";
+import { users } from "./data/users";
 
 function App() {
   const selectionCheckBoxes = useSelector((state) => state.selectionRow.value);
   console.log("selectionCheckBoxes", selectionCheckBoxes);
 
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "tags",
+      dataIndex: "tags",
+      key: "tags",
+      render: () => <div style={{ backgroundColor: "red" }}>sadsa</div>,
+    },
+    {
+      title: "action",
+      dataIndex: "action",
+      render: (cellData, column) => (
+        <button
+          style={{ backgroundColor: "red" }}
+          onClick={() => console.log("cellData", cellData)}
+        >
+          {column.title}
+        </button>
+      ),
+    },
+  ];
+
   return (
     <>
       <STable
-        data={TableProps}
-        columns={columnsTableProps}
+        data={users}
+        columns={columns}
         // align="center"
         // sıraNo={true}
         // search={true}
